73339fdd47093bcbf519a0e7c66e5d65427f6451
Guilherme Machado <guilherme.gmachado95@gmail.com>
2019-10-06 06:51:05 -0300

    Implementação do loader com interceptor de requisição.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,17 +6,25 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
 import {WeathersModule} from "./weathers";
+import {MatProgressSpinnerModule} from "@angular/material";
+import {LoaderInterceptor} from "./components/loader/loader.interceptor";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {LoaderComponent} from "./components/loader";
+import {LoaderService} from "./components/loader/loader.service";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LoaderComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         FormsModule,
-        WeathersModule
+        WeathersModule,
+        MatProgressSpinnerModule
     ],
+    providers: [LoaderService, { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true }],
     bootstrap: [AppComponent]
 })
 export class AppModule {
